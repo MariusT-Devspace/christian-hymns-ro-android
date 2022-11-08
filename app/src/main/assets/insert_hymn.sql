a123aa6078d93f1827a32ea56af3256c966933a6
Marius <mariustichieru@gmail.com>
2022-11-08 14:25:57 +0100

    Add more hymns to database

---
+++ app/src/main/assets/insert_hymn.sql
@@ -0,0 +1,9 @@
+INSERT INTO Lyrics 
+VALUES(3, 40, "Ridicaţi-vă privirea:
+Sfântă-i Maiestatea Sa, 
+Galaxii de îngeri cântă,
+Noi cu ei Îi vom cânta!
+Înspre ceruri tot aleargă
+Voci şi strigăte de dor:
+Te-aşteptăm, o, Domn al slavei,
+Vino, Domn al domnilor!", 0)
