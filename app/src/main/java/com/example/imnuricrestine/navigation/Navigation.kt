e1746a358c70bab5f6521d11c685e3852f967f75
Marius <mariustichieru@gmail.com>
2024-11-01 01:38:27 +0100

    feat(UI): use default transitions between screens

--- app/src/main/java/com/example/imnuricrestine/navigation/Navigation.kt
+++ app/src/main/java/com/example/imnuricrestine/navigation/Navigation.kt
@@ -2,13 +2,6 @@ package com.example.imnuricrestine.navigation
 
 import android.util.Log
 import androidx.activity.compose.BackHandler
-import androidx.compose.animation.core.EaseInOut
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.navigation.NavHostController
@@ -41,24 +34,6 @@ fun Navigation(
     NavHost(
         navController = navController,
         startDestination = Route.Index.route,
-        enterTransition = {
-            fadeIn(animationSpec = tween(
-                1300,
-                easing = EaseInOut
-            ))
-        },
-        exitTransition = {
-            fadeOut(animationSpec = tween(
-                1000,
-                easing = EaseInOut
-            ))
-        },
-        popEnterTransition = {
-            slideInHorizontally(initialOffsetX = { -it }, animationSpec = tween(
-                500,
-                easing = FastOutSlowInEasing
-            )) + fadeIn()
-        }
     ) {
         composable(Route.Index.route) {
             IndexScreen(
@@ -74,19 +49,7 @@ fun Navigation(
                 navArgument("hymnId"){
                     type = NavType.IntType
                 }
-            ),
-            enterTransition = {
-                slideInHorizontally(initialOffsetX = { -it }, animationSpec = tween(
-                    500,
-                    easing = FastOutSlowInEasing
-                )) + fadeIn()
-            },
-            popExitTransition = {
-                slideOutHorizontally(targetOffsetX = { -it }, animationSpec = tween(
-                    durationMillis = 400,
-                    easing = FastOutSlowInEasing
-                )) + fadeOut()
-            }
+            )
         ) { navBackStackEntry ->
             val argument = navBackStackEntry.arguments!!.getInt("hymnId")
                 HymnDetailsScreen(
