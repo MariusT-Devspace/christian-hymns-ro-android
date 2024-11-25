301e56941234966f52531b009ced68871c518fc7
Marius <mariustichieru@gmail.com>
2024-11-25 18:02:28 +0100

    refactor: improve readability on IndexScreenUiState.kt

--- app/src/main/java/com/example/imnuricrestine/state/IndexScreenUiState.kt
+++ app/src/main/java/com/example/imnuricrestine/state/IndexScreenUiState.kt
@@ -9,30 +9,41 @@ import com.example.imnuricrestine.state.PaginationConfig.TOTAL_PAGES
 import com.example.imnuricrestine.state.PaginationConfig.pages
 import kotlin.math.ceil
 
+// Constants and Utility Functions
 object PaginationConfig {
     const val PAGE_SIZE: Int = 99
     val TOTAL_PAGES: Int = ceil((765.toDouble() / PAGE_SIZE.toDouble())).toInt()
-    val pages: Array<Page> = Array(TOTAL_PAGES) { index ->
+    val pages: List<Page> = Array(TOTAL_PAGES) { index ->
         Page(index + 1)
-    }
+    }.toList()
 }
 
+private fun getPageStart(index: Int): Int =
+    if (index == 1)
+        1
+    else
+        MainActivity.hymns.value!!.find { it.index == ((index - 1) * PAGE_SIZE).toString() }!!.id + (index - 1)
+
+private fun getPageEnd(index: Int, start: Int): Int = when (index) {
+    1 -> PAGE_SIZE
+    TOTAL_PAGES ->
+        MainActivity.hymns.value!![MainActivity.hymns.value!!.size - 1].id.toInt()
+
+    else ->
+        MainActivity.hymns.value!!.indexOf(
+            MainActivity.hymns.value!!.find { it.index == (start + PAGE_SIZE).toString() }
+        ) + 1
+}
+
+private fun getPageTitle(start: Int, end: Int): String =
+    "${MainActivity.hymns.value!![start - 1].index} - ${MainActivity.hymns.value!![end - 1].index}"
+
+// Data classes, enums and type aliases
 data class Page(
     val index: Int,
-    val start: Int = if (index == 1) 1
-    else (
-        MainActivity.hymns.value!!.find{ it.index == ((index - 1) * PAGE_SIZE).toString() }!!.id
-    ) + (index - 1),
-    val end: Int = when(index) {
-        1 -> PAGE_SIZE
-        TOTAL_PAGES ->
-            MainActivity.hymns.value!![MainActivity.hymns.value!!.size - 1].id.toInt()
-        else ->
-            MainActivity.hymns.value!!.indexOf(
-                MainActivity.hymns.value!!.find { it.index == (start + PAGE_SIZE).toString() }
-        ) + 1
-    },
-    val title: String = "${MainActivity.hymns.value!![start - 1].index} - ${MainActivity.hymns.value!![end - 1].index}"
+    val start: Int = getPageStart(index),
+    val end: Int = getPageEnd(index, start),
+    val title: String = getPageTitle(start, end)
 )
 
 enum class PageChangeAction {
@@ -41,6 +52,7 @@ enum class PageChangeAction {
 
 typealias OnChangePageAction = (action: PageChangeAction, selectedPage: Int?) -> Unit
 
+// State holder class
 class IndexScreenUiState {
     private val _page: MutableState<Page> = mutableStateOf(pages[0])
     private val _paginationAppBarUiState: MutableState<PaginationAppBarUiState> = mutableStateOf(
@@ -50,17 +62,40 @@ class IndexScreenUiState {
         )
     )
 
-    private val page: State<Page> = _page
-    private val paginationAppBarUiState: State<PaginationAppBarUiState> = _paginationAppBarUiState
+    // Exposed state properties
+    val page: State<Page> = _page
+    val paginationAppBarUiState: State<PaginationAppBarUiState> = _paginationAppBarUiState
 
+    // Configure destructuring
     operator fun component1() = page
     operator fun component2() = paginationAppBarUiState
     operator fun component3() = ::onChangePageAction
 
-    private fun onChangePageAction(
+    // Update page state
+    fun onChangePageAction(
         action: PageChangeAction,
         selectedPage: Int? = null
     ) {
+        fun updatePaginationAppBarUiState() {
+            when (page.value.index) {
+                in 2..<TOTAL_PAGES ->
+                    _paginationAppBarUiState.value = paginationAppBarUiState.value.copy(
+                        isPreviousButtonEnabled = true,
+                        isNextButtonEnabled = true
+                    )
+                1 ->
+                    _paginationAppBarUiState.value = paginationAppBarUiState.value.copy(
+                        isPreviousButtonEnabled = false,
+                        isNextButtonEnabled = true
+                    )
+                TOTAL_PAGES ->
+                    _paginationAppBarUiState.value = paginationAppBarUiState.value.copy(
+                        isPreviousButtonEnabled = true,
+                        isNextButtonEnabled = false
+                    )
+            }
+        }
+
         when (action) {
             PageChangeAction.NEXT -> {
                 val index = page.value.index + 1
@@ -79,26 +114,6 @@ class IndexScreenUiState {
                 _page.value = pages[index-1]
                 updatePaginationAppBarUiState()
             }
-        }
-    }
-
-    private fun updatePaginationAppBarUiState() {
-        when (page.value.index) {
-            in 2..<TOTAL_PAGES ->
-                _paginationAppBarUiState.value = paginationAppBarUiState.value.copy(
-                    isPreviousButtonEnabled = true,
-                    isNextButtonEnabled = true
-                )
-            1 ->
-                _paginationAppBarUiState.value = paginationAppBarUiState.value.copy(
-                    isPreviousButtonEnabled = false,
-                    isNextButtonEnabled = true
-                )
-            TOTAL_PAGES ->
-                _paginationAppBarUiState.value = paginationAppBarUiState.value.copy(
-                    isPreviousButtonEnabled = true,
-                    isNextButtonEnabled = false
-                )
         }
     }
 }
