7eedc8bae4f0416ff47eaa660d6c66180a4ce989
Marius <mariustichieru@gmail.com>
2025-04-10 17:05:04 +0200

    feat(favorites): show text when favorites list is empty

--- app/src/main/java/com/example/imnuricrestine/components/Favorites.kt
+++ app/src/main/java/com/example/imnuricrestine/components/Favorites.kt
@@ -2,7 +2,10 @@ package com.example.imnuricrestine.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -31,6 +35,15 @@ import com.example.imnuricrestine.navigation.Route
 import com.example.imnuricrestine.state.FavoriteAction
 import com.example.imnuricrestine.state.FavoriteIconName
 import com.example.imnuricrestine.state.UpdateHymnsListItemUiState
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.foundation.text.appendInlineContent
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.CardColors
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.PlaceholderVerticalAlign
+import androidx.compose.ui.text.font.FontStyle
 
 @Composable
 fun Favorites(
@@ -45,67 +58,130 @@ fun Favorites(
         listState.scrollToItem(0)
     }
 
-    LazyColumn(
-        state = listState,
-        contentPadding = contentPadding,
-        modifier = Modifier.padding(top = 30.dp),
-    ) {
-        items(
-            items = favoritesListItems
-        ) { item ->
-            ListItem(
-                headlineContent = {
+    if (favoritesListItems.isNotEmpty()) {
+        LazyColumn(
+            state = listState,
+            contentPadding = contentPadding,
+            modifier = Modifier.padding(top = 30.dp),
+        ) {
+            items(
+                items = favoritesListItems
+            ) { item ->
+                ListItem(
+                    headlineContent = {
 
-                },
-                supportingContent = {
-                    Text(
-                        item.title,
-                        fontSize = 18.sp
-                    )
-                },
-                leadingContent = {
-                    Text(
-                        item.index,
-                        modifier = Modifier
-                            .background(
-                                color = MaterialTheme.colorScheme.surfaceContainerHigh,
-                                shape = MaterialTheme.shapes.extraLarge
-                            )
-                            .width(53.dp)
-                            .padding(horizontal = 5.dp, vertical = 10.dp),
-                        fontSize = 15.sp,
-                        textAlign = TextAlign.Center
-                    )
-                },
-                trailingContent = {
-                    IconButton(
-                        onClick = {
-                            onDeleteFavorite(
-                                Favorite(
-                                    item.id,
-                                    item.hymnId
-                                )
-                            ).thenRun {
-                                updateHymnsListItem(
-                                    item.hymnId.toInt() - 1,
-                                    false,
-                                    FavoriteAction.ADD_FAVORITE,
-                                    FavoriteIconName.NOT_SAVED.name
-                                )
-                            }
-                        }
-                    ) {
-                        Icon(
-                            Icons.Outlined.Favorite,
-                            contentDescription = stringResource(R.string.remove_from_favorites_description)
+                    },
+                    supportingContent = {
+                        Text(
+                            item.title,
+                            fontSize = 18.sp
                         )
+                    },
+                    leadingContent = {
+                        Text(
+                            item.index,
+                            modifier = Modifier
+                                .background(
+                                    color = MaterialTheme.colorScheme.surfaceContainerHigh,
+                                    shape = MaterialTheme.shapes.extraLarge
+                                )
+                                .width(53.dp)
+                                .padding(horizontal = 5.dp, vertical = 10.dp),
+                            fontSize = 15.sp,
+                            textAlign = TextAlign.Center
+                        )
+                    },
+                    trailingContent = {
+                        IconButton(
+                            onClick = {
+                                onDeleteFavorite(
+                                    Favorite(
+                                        item.id,
+                                        item.hymnId
+                                    )
+                                ).thenRun {
+                                    updateHymnsListItem(
+                                        item.hymnId.toInt() - 1,
+                                        false,
+                                        FavoriteAction.ADD_FAVORITE,
+                                        FavoriteIconName.NOT_SAVED.name
+                                    )
+                                }
+                            }
+                        ) {
+                            Icon(
+                                Icons.Outlined.Favorite,
+                                contentDescription = stringResource(R.string.remove_from_favorites_description)
+                            )
+                        }
+                    },
+                    modifier = Modifier.clickable {
+                        val hymnId = item.hymnId.toInt() - 1
+                        navController.navigate(Route.HymnDetails.route+"/${hymnId}")
+                    },
+                )
+            }
+        }
+    } else {
+        Box(modifier = Modifier.padding(vertical = 80.dp, horizontal = 16.dp),
+            contentAlignment = Alignment.Center) {
+            Column {
+                Text(
+                    stringResource(R.string.favorites_empty_heading),
+                    modifier = Modifier.padding(top = 16.dp),
+                    fontSize = 28.sp,
+                    lineHeight = 32.sp,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+
+                Text(
+                    stringResource(R.string.favorites_empty_content),
+                    modifier = Modifier.padding(top = 16.dp),
+                    fontSize = 18.sp,
+                    fontStyle = FontStyle.Italic,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+                Card(
+                    modifier = Modifier.padding(top = 46.dp),
+                    colors = CardColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainerLow,
+                        contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                        disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        disabledContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                ) {
+                    val favoritesIconId = "favoritesIconId"
+                    val saveToFavoritesInstructionsText = buildAnnotatedString {
+                        append("Apasă pe ")
+                        appendInlineContent(favoritesIconId, "[icon]")
+                        append(" pentru a adăuga un imn la favorite.")
                     }
-                },
-                modifier = Modifier.clickable {
-                    val hymnId = item.hymnId.toInt() - 1
-                    navController.navigate(Route.HymnDetails.route+"/${hymnId}")
-                },
-            )
+
+                    val inlineContent = mapOf(
+                        Pair(
+                            favoritesIconId,
+                            InlineTextContent(
+                                Placeholder(
+                                    width = 20.sp,
+                                    height = 20.sp,
+                                    placeholderVerticalAlign = PlaceholderVerticalAlign.Center
+                                )
+                            ) {
+                                Icon(Icons.Filled.Favorite,"", modifier = Modifier.fillMaxSize())
+                            }
+                        )
+                    )
+
+                    Text(
+                        saveToFavoritesInstructionsText,
+                        inlineContent = inlineContent,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+
         }
     }
+
 }
