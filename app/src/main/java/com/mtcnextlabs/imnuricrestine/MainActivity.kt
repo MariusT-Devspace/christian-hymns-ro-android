aa259f6ad42d8762512bc97c7a89591ca8c60b83
MariusT-Devspace <mariustichieru@gmail.com>
2026-01-20 00:46:32 +0100

    feat(UI): make navigation bar transparent

--- app/src/main/java/com/mtcnextlabs/imnuricrestine/MainActivity.kt
+++ app/src/main/java/com/mtcnextlabs/imnuricrestine/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.mtcnextlabs.imnuricrestine
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.mtcnextlabs.imnuricrestine.ui.HymnsApp
 import com.mtcnextlabs.imnuricrestine.ui.theme.ChristianHymnsTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,6 +14,16 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.auto(
+                lightScrim = android.graphics.Color.TRANSPARENT,
+                darkScrim = android.graphics.Color.TRANSPARENT
+            )
+        )
+        // FORCE disable the grey overlay (Contrast Enforcement)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.isNavigationBarContrastEnforced = false
+        }
 
         setContent {
             ChristianHymnsTheme {
